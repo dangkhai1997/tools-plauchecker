67d22a82b7bf188cd6d53036700fab16c3cc54fa
Khai Nguyen <khaind1997@gmail.com>
2025-07-07 22:39:50 +0700

    add duc plause

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: "Seven", url: "https://plausible.io/share/noje.intelnestle.com?f=contains,page,dk74&auth=88OeC8D9rkNyPm_ytzMo9" },
         { name: "Bỉ", url: "https://plausible.io/share/sportnieuws.fusiondigest.com?f=contains,page,dk74&auth=V9aSX0TDQ9PEz-LDOXN9A" },
         { name: "Hà Lan", url: "https://plausible.io/share/nieuws.intelnestle.com?f=contains,page,dk74&auth=lx0fI1jLk6OG8Cj8MkgNi" },
-        { name: "Đức", url: "https://plausible.io/share/live.fusiondigest.com?f=contains,page,dk74&auth=xyz" } // Đã thêm trang Đức
+        { name: "Đức", url: "https://plausible.io/share/live.fusiondigest.com?f=contains,page,dk74&auth=ixRP0ymfRz9weZgCEfO-c" } // Đã thêm trang Đức
     ];
 
     // Biến toàn cục để lưu trữ dữ liệu bài viết đang hiển thị và trạng thái sắp xếp
